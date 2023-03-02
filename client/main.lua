fac45d4d4f15e259802d3f85ecef5409ca95a823
Agimir <william@mesaindigostudios.com>
2023-03-01 23:10:55 -0600

    Good god my eyes
    the blip works the blip works the blip works the blip works the blip works the blip works the blip works the blip works the blip works the blip works the blip works the blip works

--- client/main.lua
+++ client/main.lua
@@ -1,82 +1,41 @@
-local table = lib.table
-InService = player.inService and 
-table.contains(Config.JobGroup, player.inService) 
-and player.hasGroup(Config.JobGroup)
+-- Local variables
+local playerload = LocalPlayer.state.isLoggedIn
 
-local taskLoc = nil
-local taskped = {
-    spawned = false,
-    ped = nil
-}
-local currenttask = nil
-local isdoingtask = nil
+-- Job HQ Blip
+local blips = {
+    {title= Config.job_blip.name,
+    colour=Config.job_blip.color,
+    id=Config.job_blip.sprite,
+    x = Config.job_blip.location.x,
+    y = Config.job_blip.location.y,
+    scale = Config.job_blip.scale}
+		}
+Citizen.CreateThread(function()
 
-local task1 = Config.TaskOneOps[math.random(1, #Config.TaskOneOps)].totasklocation
--- Is possible to use NPWD message export for job? https://projecterror.dev/docs/
+    for _, info in pairs(blips) do
+      info.blip = AddBlipForCoord(info.x, info.y)
+      SetBlipSprite(info.blip, info.id)
+      SetBlipDisplay(info.blip, 4)
+      SetBlipScale(info.blip, info.scale)
+      SetBlipColour(info.blip, info.colour)
+	  BeginTextCommandSetBlipName("STRING")
+      AddTextComponentString(info.title)
+      EndTextCommandSetBlipName(info.blip)
+    end
+end)
+-- Job HQ Model
+
 
 local function createTaskLocation()
-    if taskLoc ~= nil then
-        RemoveBlip(taskLoc)
-        taskLoc = nil
-    end
-    taskLoc = AddBlipForCoord(currenttask.totasklocation.x, 
-        currenttask.totasklocation.y, 
-        currenttask.totasklocation.z)
-    SetBlipSprite(taskLoc, 1)
-    SetBlipColour(taskLoc, 27)
-    SetBlipRoute(taskLoc, true)
-    SetBlipRouteColour(taskLoc, 27)
-    SetBlipScale(taskLoc, 0.8)
-    BeginTextCommandSetBlipName('STRING')
-    AddTextComponentString('Task One')
-    EndTextCommandSetBlipName(taskLoc)
-end
 
-local function createTask_Ped()
-    local model = joaat('a_f_m_beach_01')
-    lib.requestModel(model)
-
-    currenttask = task1
-
-    local ped = CreatePed(0, model, currenttask.x, 
-        currenttask.y, currenttask.z-1, currenttask.w, false, false)
-
-    taskped.ped = ped
-
-    TaskStartScenarioInPlace(ped, 'PROP_HUMAN_STAND_IMPATIENT', 0, true)
-    FreezeEntityPosition(ped, true)
-    SetEntityInvincible(ped, true)
-    SetBlockingOfNonTemporaryEvents(ped, true)
-
-    local options = {
-        {
-            name = 'mioxjob:doingtask',
-            event = 'mioxjob:doingtask',
-            icon = 'fa-solid fa-toolbox',
-            label = 'Do Task',
-            canInteract = function(_, distance)
-                return distance < 2.0
-            end
-        }
-    }
-    exports.ox_target:addLocalEntity(taskped.ped, options)
-    taskped.spawned = true
 end
 
 local function removeTask_Ped()
-    exports.ox_target:removeLocalEntity(taskped.ped, { 'mioxjob:doingtask' })
-    DeleteEntity(taskped.ped)
-    taskped.spawned = false
-    taskped.ped = nil
+
 end
 
 RegisterNetEvent('mioxjob:start_taskone', function()
-    local task1 = Config.TaskOneOps[math.random(1, #Config.TaskOneOps)]
-    currenttask = task1
-
     createTaskLocation()
-    createTask_Ped()
-
 end)
 
 RegisterNetEvent('mioxjob:doingtask', function()
@@ -95,4 +54,11 @@ RegisterNetEvent('mioxjob:doingtask', function()
         TriggerServerEvent('mioxjob:taskcompleted')
         job_paidnotification()
     end
+end)
+
+-- Resource management
+AddEventHandler('onResourceStart', function(resource)
+    if resource ~= GetCurrentResourceName() then return end
+    if not playerload then return end
+    Wait(100)
 end)
--- config.lua
+++ config.lua
@@ -3,14 +3,23 @@ Config.Debug = true
 
 Config.JobGroup = { 'job1' }
 
-Config.TaskOneOps = {
+Config.job_blip = {
+    location = vector2(910.619, -2119.245),
+    sprite = 1,
+    color = 5,
+    scale = 0.8,
+    name = 'JOB HQ'
+}
+
+Config.job_hq = {
+    model = 'mp_s_m_armoured_01',
+    ped_loc = vector4(906.439, -2123.929, 31.23, 356.29)
+}
+
+Config.dothetask = {
+    -- need = location, ped location
     [1] = {
-        -- Locations
-        totasklocation = vector4(397.815, 64.673, 97.977, 356.114),
-        taskobjective = vector3(398.157, 67.518, 97.977),
-        -- Model
-        taskped = 'a_f_m_beach_01',
-        taskpayout = math.random(100,300)
+        ['task_loc'] = vector4(954.433, -1964.781, 30.42, 131.045),
+        ['ped_loc'] = vector(958.1, -1968.27, 30.3, 61.036)
     }
-    
 }
